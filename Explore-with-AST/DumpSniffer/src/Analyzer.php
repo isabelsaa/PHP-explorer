580005200838b63008104aed22b71a669a9830cc
isabelsaa <isabel89@protonmail.com>
2025-07-01 21:49:55 +0200

    [UPDATE] Add composer into the Dumpsniffer

--- Explore-with-AST/DumpSniffer/src/Analyzer.php
+++ Explore-with-AST/DumpSniffer/src/Analyzer.php
@@ -2,63 +2,68 @@
 
 declare(strict_types=1);
 
-const FUNCTION_DECLARATION_KIND = ast\AST_FUNC_DECL;
-const ECHO_KIND = ast\AST_ECHO;
-const FUNCTION_NAME_KIND = ast\AST_NAME;
-const FUNCTION_CALL_KIND = ast\AST_CALL;
+namespace DumpSniffer;
 
-const PROHIBITED_FUNCTIONS = ['dd', 'var_dump'];
-
-function isFunctionDeclaration(ast\Node $node): bool
+final class Analyzer
 {
-    return $node->kind === FUNCTION_DECLARATION_KIND;
-}
+    private const FUNCTION_DECLARATION_KIND = \ast\AST_FUNC_DECL;
+    private const ECHO_KIND = \ast\AST_ECHO;
+    private const FUNCTION_NAME_KIND = \ast\AST_NAME;
+    private const FUNCTION_CALL_KIND = \ast\AST_CALL;
 
-function isProhibitedFunctionCall(mixed $node): ?array
-{
-    if ($node->kind !== FUNCTION_CALL_KIND) {
+    private const array PROHIBITED_FUNCTIONS = ['dd', 'var_dump'];
+
+    public function isFunctionDeclaration(\ast\Node $node): bool
+    {
+        return $node->kind === self::FUNCTION_DECLARATION_KIND;
+    }
+
+    public function isProhibitedFunctionCall(mixed $node): ?array
+    {
+        if ($node->kind !== self::FUNCTION_CALL_KIND) {
+            return null;
+        }
+
+        $nameNode = $node->children['expr'] ?? null;
+        if (
+            $nameNode instanceof \ast\Node
+            && $nameNode->kind === self::FUNCTION_NAME_KIND
+        ) {
+            $functionName = strtolower($nameNode->children['name']);
+
+            if (in_array($functionName, self::PROHIBITED_FUNCTIONS, true)) {
+                return [
+                    'message' => "There is a \"$functionName()\" detected, please remove it",
+                    'lineno' => $node->lineno,
+                ];
+            }
+        }
+
         return null;
+
     }
 
-    $nameNode = $node->children['expr'] ?? null;
-    if (
-        $nameNode instanceof ast\Node
-        && $nameNode->kind === FUNCTION_NAME_KIND
-    ) {
-        $functionName = strtolower($nameNode->children['name']);
+    public function analyzeAst(mixed $node, bool $isInsideFunctionScope = false): \Generator
+    {
+        if ($node instanceof \ast\Node) {
+            if ($this->isFunctionDeclaration($node)) {
+                $isInsideFunctionScope = true;
+            }
 
-        if (in_array($functionName, PROHIBITED_FUNCTIONS, true)) {
-            return [
-                'message' => "There is a \"$functionName()\" detected, please remove it",
-                'lineno' => $node->lineno,
-            ];
-        }
-    }
+            if ($node->kind === self::ECHO_KIND && $isInsideFunctionScope) {
+                yield [
+                    'message' => 'There is an "echo", please remove it.',
+                    'lineno' => $node->lineno,
+                ];
+            }
 
-    return null;
+            if ($issue = $this->isProhibitedFunctionCall($node)) {
+                yield $issue;
+            }
 
-}
-
-function analyzeAst(mixed $node, bool $isInsideFunctionScope = false): Generator
-{
-    if ($node instanceof ast\Node) {
-        if (isFunctionDeclaration($node)) {
-            $isInsideFunctionScope = true;
-        }
-
-        if ($node->kind === ECHO_KIND && $isInsideFunctionScope) {
-            yield [
-                'message' => 'There is an "echo", please remove it.',
-                'lineno' => $node->lineno,
-            ];
-        }
-
-        if ($issue = isProhibitedFunctionCall($node)) {
-            yield $issue;
-        }
-
-        foreach ($node->children as $child) {
-            yield from analyzeAst($child, $isInsideFunctionScope);
+            foreach ($node->children as $child) {
+                yield from $this->analyzeAst($child, $isInsideFunctionScope);
+            }
         }
     }
 }
--- Explore-with-AST/DumpSniffer/src/Code.php
+++ Explore-with-AST/DumpSniffer/src/Code.php
@@ -1,9 +1,12 @@
 <?php
+
+declare(strict_types=1);
+
 function helloWorld(string $name): void
 {
     echo 'Hello'. $name;
-    dd($name);
     var_dump($name);
+    dd($name);
 }
 
 helloWorld('Mary');
