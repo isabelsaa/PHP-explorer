154e8b85ae26f14c01119960eb3fb1e1415e4166
isabelsaa <isabel89@protonmail.com>
2025-10-27 19:32:21 +0100

    [UPDATE] - Comparing time between optimized version and normal version of bubble sort

--- Explore-algorithms/Sort-algorithms/bubble-sort.php
+++ Explore-algorithms/Sort-algorithms/bubble-sort.php
@@ -7,13 +7,57 @@ function bubbleSort(&$elements): void
         for ($index = 0; $index < $length - 1 - $iteration; $index++) {
             if ($elements[$index] > $elements[$index + 1]) {
                 [$elements[$index], $elements[$index + 1]] = [$elements[$index + 1], $elements[$index]];
-                print_r($elements);
+                //print_r($elements);
             }
         }
     }
 }
 
-$elements = [4, 7, 6, 2, 1];
+function bubbleSortOptimized(&$elements): void
+{
+    $length = count($elements);
+    for ($iteration = 0; $iteration < $length - 1; $iteration++) {
+        $swapped = false;
+        for ($index = 0; $index < $length - 1 - $iteration; $index++) {
+            if ($elements[$index] > $elements[$index + 1]) {
+                [$elements[$index], $elements[$index + 1]] = [$elements[$index + 1], $elements[$index]];
+                $swapped = true;
+                //print_r($elements);
+            }
+        }
+        if (!$swapped) {
+            break;
+        }
+    }
+}
+
+function showArrayState(string $value, array $elements): void
+{
+    echo $value . ': [' . implode(', ', $elements) . "]\n";
+}
+
+$original = [3, 1, 2, 4, 7, 8, 1, 9];
+
+echo "=== Bubble Sort (normal) ===\n";
+showArrayState('Desordenado', $original);
+
+$elements = $original;
+$start = microtime(true);
 bubbleSort($elements);
-echo "RESULT\n";
-print_r($elements);
+$end = microtime(true);
+$executionTime = $end - $start;
+
+showArrayState('Ordenado', $elements);
+printf("Tiempo de ejecución: %.6f segundos\n\n", $executionTime);
+
+echo "=== Bubble Sort (optimizado) ===\n";
+showArrayState('Desordenado', $original);
+
+$elements = $original;
+$start = microtime(true);
+bubbleSortOptimized($elements);
+$end = microtime(true);
+$executionTime = $end - $start;
+
+showArrayState('Ordenado', $elements);
+printf("Tiempo de ejecución: %.6f segundos\n", $executionTime);
